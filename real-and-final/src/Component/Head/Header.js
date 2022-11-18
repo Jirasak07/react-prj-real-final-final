c71014fabf8e904af89f097748575247a88b24d2
Jirasak07 <bee.110243@gmail.com>
2022-11-18 14:44:07 +0700

    create product

--- real-and-final/src/Component/Head/Header.js
+++ real-and-final/src/Component/Head/Header.js
@@ -1,16 +1,21 @@
 import React, { useState } from "react";
 import { FaBars } from "react-icons/fa";
 import { MDBRow, MDBCol, MDBContainer } from "mdbreact";
+import { IoMdArrowDropdown } from "react-icons/io";
 import "bootstrap/dist/css/bootstrap.min.css";
-import {FaHome} from 'react-icons/fa'
-import {HiDocumentText} from 'react-icons/hi'
-import {HiBuildingOffice2} from 'react-icons/hi2'
-import logokpru from '../../image/LOGO RGB PNG-สำหรับงานนำเสนอแบบดิจิติล.png' 
-import {FaUsersCog} from 'react-icons/fa'
+import { FaHome } from "react-icons/fa";
+import { HiDocumentText } from "react-icons/hi";
+import { HiBuildingOffice2 } from "react-icons/hi2";
+import logokpru from "../../image/LOGO RGB PNG-สำหรับงานนำเสนอแบบดิจิติล.png";
+import { FaUsersCog } from "react-icons/fa";
 import "./Head.css";
 import { NavLink } from "react-router-dom";
 function Header() {
   const [side, setSide] = useState(false);
+  const [dd, setDd] = useState(false);
+  const dropdown = () => {
+    setDd(!dd);
+  };
   return (
     <div className="app">
       <div
@@ -20,45 +25,59 @@ function Header() {
       <div className="nav-head">
         <MDBContainer fluid>
           <MDBRow className="boxnav">
-            <MDBCol
-              size="1"
-              className=" h-100 bars d-flex align-items-center "
-            >
+            <MDBCol size="1" className=" h-100 bars d-flex align-items-center ">
               <FaBars onClick={(closSide) => setSide(!side)} />
             </MDBCol>
-            <MDBCol size="11" md="9" className=" d-flex justify-content-center  align-items-center  logo h-100 ">
-            <img src={logokpru} width='30px' /> &nbsp;  ระบบตรวจสอบครุภัณฑ์ สำนักส่งเสริมวิชาการและงานทะเบียน
+            <MDBCol
+              size="11"
+              md="9"
+              className=" d-flex justify-content-center  align-items-center  logo h-100 "
+            >
+              <img src={logokpru} width="30px" /> &nbsp; ระบบตรวจสอบครุภัณฑ์
+              สำนักส่งเสริมวิชาการและงานทะเบียน
             </MDBCol>
-            <MDBCol className="   name-d h-100 ">
-              ชื่อผู้ใช้ icon dropdown
+            <MDBCol className="   name-d h-100 mx-3">
+              <div className="name">ชื่อผู้ใช้</div>
+              <IoMdArrowDropdown
+                onClick={dropdown}
+                className={dd ? "up active " : "up"}
+              />
             </MDBCol>
           </MDBRow>
         </MDBContainer>
       </div>
       <div className={side ? "sidebar" : "sidebar clos"}>
         <div className="sidenav-body">
-          <div className="menu-title" >MENU</div>
+          <div className="menu-title">MENU</div>
           <div className="nav-item">
             <NavLink to="/product" className="mx-3 cusFont">
-              <div className="icon"><FaHome/></div>
+              <div className="icon">
+                <FaHome />
+              </div>
               <div className="menu">ครุภัณฑ์</div>
             </NavLink>
           </div>
           <div className="nav-item">
             <NavLink to="/agen" className="mx-3 cusFont">
-              <div className="icon"><HiBuildingOffice2/></div>
+              <div className="icon">
+                <HiBuildingOffice2 />
+              </div>
               <div className="menu">หน่วยงาน</div>
             </NavLink>
           </div>
           <div className="nav-item">
             <NavLink to="/user" className="mx-3 cusFont">
-              <div className="icon"><FaUsersCog/></div>
+              <div className="icon">
+                <FaUsersCog />
+              </div>
               <div className="menu">เจ้าหน้าที่</div>
             </NavLink>
           </div>{" "}
           <div className="nav-item">
             <NavLink to="/report" className="mx-3 cusFont">
-              <div className="icon"><HiDocumentText/></div>
+              <div className="icon">
+                <HiDocumentText />
+              </div>
               <div className="menu">ออกรายงาน</div>
             </NavLink>
           </div>
--- real-and-final/src/Router.js
+++ real-and-final/src/Router.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Product from './Component/Product/Product'
+import User_Add from "./Component/User/User_Add";
 
 function Router() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Product/>} />
+        <Route path="/product" element={<Product/>} />
+        <Route path="/user" element={<User_Add/>} />
       </Routes>
     </div>
   );
