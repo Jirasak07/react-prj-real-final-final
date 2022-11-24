993d80574ab9f23ea789a994b566340080252de2
Jirasak07 <bee.110243@gmail.com>
2022-11-24 09:17:16 +0700

    621320907

---
+++ 621320907/src/App.js
@@ -0,0 +1,12 @@
+import React,{useEffect} from 'react'
+
+function App() {
+  useEffect(()=>{
+    document.body.style.backgroundColor = " #434242"
+  })
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
