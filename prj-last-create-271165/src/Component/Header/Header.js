79e5fadf6db7e4a21be5bd02e00d41c01f992cd1
Jirasak07 <bee.110243@gmail.com>
2022-11-27 23:09:17 +0700

    good

---
+++ prj-last-create-271165/src/Component/Header/Header.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import "./Header.css";
+import { AiOutlineBars } from "react-icons/ai";
+import {HiOutlineDesktopComputer} from 'react-icons/hi'
+import {AiOutlineScan} from 'react-icons/ai'
+import {BiBuildingHouse,BiUser,BiLogOut} from 'react-icons/bi'
+import {TbReportAnalytics} from 'react-icons/tb'
+
+function Header() {
+  const [side, setSide] = useState(true);
+  const clickside = () => {
+    setSide(!side);
+  };
+  return (
+    <div>
+      <div className={side ? "bg-overlay  " : "bg-overlay active"}></div>
+      <div className="navhead">
+        <div className="nav iconbar icon col-1 " onClick={clickside}>
+          <AiOutlineBars />
+        </div>
+        <div className="nav headname col ">ระบบตรวจสอบครุภัณฑ์</div>
+      </div>
+      {/*  SidebarNav  */}
+
+      <div className={side ? "sidebar active" : "sidebar"}>
+        <div className="open-icon icon" onClick={clickside}>
+          <AiOutlineBars />
+        </div>
+        <div className="group-nav " >
+          <div className="item"><HiOutlineDesktopComputer className="ms" /> &nbsp; ครุภัณฑ์</div>
+          <div className="item"><AiOutlineScan className="ms" />&nbsp; สแกน</div>
+          <div className="item"><BiBuildingHouse className="ms" />&nbsp; หน่วยงาน</div>
+          <div className="item"><BiUser className="ms"/>&nbsp; เจ้าหน้าที่</div>
+          <div className="item"><TbReportAnalytics className="ms"/>&nbsp; ออกรายงาน</div>
+          <div className="item logout"><BiLogOut className="ms" />&nbsp; ออกจากระบบ</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ prj-last-create-271165/src/Component/Page/Product/Product.js
@@ -0,0 +1,50 @@
+import { MDBDataTable } from "mdbreact";
+import React, { useState } from "react";
+import "./StyleProduct.css";
+function Product() {
+    const [data, setData] = useState({
+        columns:[
+            {
+                label:"หมายเลขครุภัณฑ์",
+                field:"pid"
+            },
+            {
+                label:"รายการ",
+                field:"pname"
+            },
+            {
+                label:"หน่วยงานที่ติดตั้ง",
+                field:"sname"
+            },
+            {
+                label:"สถานะ",
+                field:"pstatus_name"
+            },
+            {
+                label:"หมายเลขครุภัณฑ์",
+                field:"pid"
+            },
+            {
+                label:"หมายเลขครุภัณฑ์",
+                field:"pid"
+            },
+            {
+                label:"หมายเลขครุภัณฑ์",
+                field:"pid"
+            },
+            {
+                label:"หมายเลขครุภัณฑ์",
+                field:"pid"
+            },
+            
+        ]
+    })
+  return (
+    <div className="product">
+      <div className="container-sm">
+        <MDBDataTable sortable={false} theadColor="dark" data={data}/></div>
+    </div>
+  );
+}
+
+export default Product;
